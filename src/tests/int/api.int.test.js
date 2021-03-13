60b0423f02fd245435c2ff2936aa0a741baa2689
Maria Tibau <maria.ta.89@gmail.com>
2021-03-13 18:42:05 +0100

    test should be called api using set param

--- src/tests/int/api.int.test.js
+++ src/tests/int/api.int.test.js
@@ -1,9 +1,13 @@
 import { getImages } from "../../api/api"
 import mockAxios from "axios"
-import { SUCCESFUL_RESPONSE_WITH_DEFAULT_PARAMS } from "./data/responses"
+import { 
+    SUCCESFUL_RESPONSE_WITH_DEFAULT_PARAMS,
+    SUCCESFUL_RESPONSE_WITH_PAGE_PARAM
+} from "./data/responses"
 jest.mock("axios")
 
 const URL_WITH_DEFAULT_PARAMS = "https://jsonplaceholder.typicode.com/photos?_limit=10&_page=1"
+const URL_WITH_PAGE_PARAM = "https://jsonplaceholder.typicode.com/photos?_limit=10&_page=2"
 
 describe('retrieve images', () => {
 
@@ -16,5 +20,18 @@ describe('retrieve images', () => {
         expect(mockFn).toBeCalledWith(URL_WITH_DEFAULT_PARAMS)
 
     })
+
+    it('should be called api using a set page param', async () => {
+        const mockFn = mockAxios.get.mockResolvedValue(SUCCESFUL_RESPONSE_WITH_PAGE_PARAM)
+
+        const PAGE_NUM = 2;
+        await getImages(PAGE_NUM)
+
+        expect(mockFn).toBeCalledTimes(1)
+        expect(mockFn).toBeCalledWith(URL_WITH_PAGE_PARAM)
+
+    })
+
+    afterEach(() => jest.clearAllMocks)
 
 })
