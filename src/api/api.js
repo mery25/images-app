e99d6c53d21a911b4fd7c17f5987c92d774177d5
Maria Tibau <maria.ta.89@gmail.com>
2021-03-15 20:30:29 +0100

    add id on the image mapped from api

--- src/api/api.js
+++ src/api/api.js
@@ -4,7 +4,7 @@ export async function getImages(page = 1, limit = 10) {
     try {
         const response = await axios.get(`https://jsonplaceholder.typicode.com/photos?_limit=${limit}&_page=${page}`)
         const data =  await response.data
-        return data.map(({ url : src, title: alt }) => ({ src, alt }))
+        return data.map(({ id, url : src, title: alt }) => ({ id, src, alt }))
     } catch(error) {
         console.log(error)
         throw new Error("Error retrieving images")
