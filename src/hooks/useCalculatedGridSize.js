1feded512984a63d766c214c6358bab894bad789
Maria Tibau <maria.ta.89@gmail.com>
2021-03-19 22:31:06 +0100

    move updateSize outside the useEffect

--- src/hooks/useCalculatedGridSize.js
+++ src/hooks/useCalculatedGridSize.js
@@ -2,41 +2,48 @@ import { useLayoutEffect, useState } from "react";
 import PropTypes from 'prop-types';
 
 const useCalculateGridSize = (maxWidthItem = 200, itemPadding = 8) => {
-  const [size, setSize] = useState([0, 0]);
+  const [dimensions, setDimensions] = useState([0, 0]);
+  const [sizeItem, setSizeItem] = useState(0);
 
-  const calculateNumCols = (widthScreen) => {
-    return Math.round(widthScreen / maxWidthItem);
-  };
-
-  const calculateNumRows = (numCols, innerWidth, innerHeight, padding) => {
-    const availWidth = innerWidth - padding * (numCols + 1);
-    const maxWidthImg = availWidth / numCols;
-    return Math.ceil(innerHeight / maxWidthImg);
-  };
-
-  const calculateHeightImgWrapper = (innerHeight, numRows, padding) => {
-    const innerHeightWithPadding = innerHeight / numRows;
-    const totalPadingImg = padding * (numRows + 1) / numRows;
-    const heightImg = innerHeightWithPadding - totalPadingImg;
-    return Math.round((heightImg + Number.EPSILON) * 100) / 100;
-  };
-
-  useLayoutEffect(() => {
-    const updateSize = () => {
-      setSize([window.innerWidth, window.innerHeight]);
+  const updateSize = () => {
+    console.log("useCallback updateSize")
+    const calculateNumCols = (widthScreen) => {
+      return Math.round(widthScreen / maxWidthItem);
+    };
+  
+    const calculateNumRows = (numCols, innerWidth, innerHeight, padding) => {
+      const availWidth = innerWidth - padding * (numCols + 1);
+      const maxWidthImg = availWidth / numCols;
+      return Math.ceil(innerHeight / maxWidthImg);
+    };
+  
+    const calculateHeightImgWrapper = (innerHeight, numRows, padding) => {
+      const innerHeightWithPadding = innerHeight / numRows;
+      const totalPadingImg = padding * (numRows + 1) / numRows;
+      const heightImg = innerHeightWithPadding - totalPadingImg;
+      return Math.round((heightImg + Number.EPSILON) * 100) / 100;
     };
 
+    const { innerWidth, innerHeight } = window
+    const numCols = calculateNumCols(innerWidth);
+    const numRows = calculateNumRows(numCols, innerWidth,innerHeight, itemPadding);
+    setDimensions([numRows, numCols])
+    const sizeItem = calculateHeightImgWrapper(innerHeight, numRows, itemPadding);
+    setSizeItem(sizeItem);
+  };
+
+
+  useLayoutEffect(() => {
+    console.log("useLayoutEffect updateSize")
     window.addEventListener("resize", updateSize);
     updateSize();
     return () => window.removeEventListener("resize", updateSize);
   }, []);
 
-  const numCols = calculateNumCols(size[0]);
-  const numRows = calculateNumRows(numCols, size[0], size[1], itemPadding);
-  const sizeItem = calculateHeightImgWrapper(size[1], numRows, itemPadding);
+
   return {
-    numCols: numCols,
-    numRows: numRows,
+    numCols: dimensions[1],
+    numRows: dimensions[0],
     sizeItem: sizeItem,
   };
 };
