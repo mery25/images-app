55cc064762b60d32ed155f938f5cd19c581ac8c6
Maria Tibau <maria.ta.89@gmail.com>
2021-03-21 14:15:59 +0100

    test api call with start param instead of page param

--- src/tests/int/api.int.test.js
+++ src/tests/int/api.int.test.js
@@ -2,7 +2,7 @@ import { getImages } from "../../api/api"
 import mockAxios from "axios"
 import { 
     SUCCESFUL_RESPONSE_WITH_DEFAULT_PARAMS,
-    SUCCESFUL_RESPONSE_WITH_PAGE_PARAM,
+    SUCCESFUL_RESPONSE_WITH_START_PARAM,
     SUCCESFUL_RESPONSE_WITH_LIMIT_PARAM,
     SUCCESFUL_RESPONSE,
     ERROR_RESPONSE
@@ -10,10 +10,10 @@ import {
 
 jest.mock("axios")
 
-const URL_WITH_DEFAULT_PARAMS = "https://jsonplaceholder.typicode.com/photos?_limit=10&_page=1"
-const URL_WITH_PAGE_PARAM = "https://jsonplaceholder.typicode.com/photos?_limit=10&_page=2"
-const URL_WITH_LIMIT_PARAM = "https://jsonplaceholder.typicode.com/photos?_limit=3&_page=1"
-const DEFAULT_PAGE_PARAM = 1;
+const URL_WITH_DEFAULT_PARAMS = "https://jsonplaceholder.typicode.com/photos?_limit=10&_start=0"
+const URL_WITH_START_PARAM = "https://jsonplaceholder.typicode.com/photos?_limit=10&_start=10"
+const URL_WITH_LIMIT_PARAM = "https://jsonplaceholder.typicode.com/photos?_limit=3&_start=0"
+const DEFAULT_START_PARAM = 0;
 
 describe('retrieve images', () => {
 
@@ -27,14 +27,14 @@ describe('retrieve images', () => {
 
     })
 
-    it('should be called api passing page param', async () => {
-        const mockFn = mockAxios.get.mockResolvedValue(SUCCESFUL_RESPONSE_WITH_PAGE_PARAM)
+    it('should be called api passing start param', async () => {
+        const mockFn = mockAxios.get.mockResolvedValue(SUCCESFUL_RESPONSE_WITH_START_PARAM)
 
-        const PAGE_NUM = 2;
-        await getImages(PAGE_NUM)
+        const START_NUM = 10;
+        await getImages(START_NUM)
 
         expect(mockFn).toBeCalledTimes(1)
-        expect(mockFn).toBeCalledWith(URL_WITH_PAGE_PARAM)
+        expect(mockFn).toBeCalledWith(URL_WITH_START_PARAM)
 
     })
 
@@ -42,7 +42,7 @@ describe('retrieve images', () => {
         const mockFn = mockAxios.get.mockResolvedValue(SUCCESFUL_RESPONSE_WITH_LIMIT_PARAM)
 
         const LIMIT_NUM = 3;
-        await getImages(DEFAULT_PAGE_PARAM, LIMIT_NUM)
+        await getImages(DEFAULT_START_PARAM, LIMIT_NUM)
 
         expect(mockFn).toBeCalledTimes(1)
         expect(mockFn).toBeCalledWith(URL_WITH_LIMIT_PARAM)
@@ -53,8 +53,8 @@ describe('retrieve images', () => {
         mockAxios.get.mockResolvedValue(SUCCESFUL_RESPONSE)
 
         const LIMIT = 2;
-        const PAGE = 3;
-        const images = await getImages(PAGE, LIMIT)
+        const START = 20;
+        const images = await getImages(START, LIMIT)
 
         expect(images).not.toBeNull()
         expect(images.length).toBe(LIMIT)
